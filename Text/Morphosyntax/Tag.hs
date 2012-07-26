2a866edb8d609cd6f1856888802658c96ff803db
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-07-26 18:00:47 +0200

    Correct handling of tags with optional attributes at the end

--- Text/Morphosyntax/Tag.hs
+++ Text/Morphosyntax/Tag.hs
@@ -23,6 +23,7 @@ parseTag tagset inp =
             ++ " no value for " ++ L.unpack attr
             ++ " attribute in tag " ++ L.unpack inp
     parseRule [] [] = []
+    parseRule ((_, True):atts) [] = parseRule atts []
     parseRule as [] = error $ "parseRule: unexpected end of input in tag "
         ++ L.unpack inp
     parseRule [] xs = error $ "parseRule: input too long in tag "
