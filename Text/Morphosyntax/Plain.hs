5342eb6d16ebb85d5b96e898c399e9f472de9d65
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-07-29 00:30:06 +0200

    Printing plain data and tags

--- Text/Morphosyntax/Plain.hs
+++ Text/Morphosyntax/Plain.hs
@@ -6,16 +6,24 @@ module Text.Morphosyntax.Plain
 , parseSent
 , parsePlainR
 , parseSentR
+
+, showPlain
+, showSent
+, showWord
 ) where
 
+import Data.Monoid (Monoid, mempty, mappend, mconcat)
 import Data.Maybe (catMaybes)
 import Data.List (groupBy, intercalate)
 import qualified Data.Text.Lazy as L
+import qualified Data.Text.Lazy.Builder as L
+import qualified Data.Set as S
 
 import Data.Morphosyntax.Base (Space (..))
 import Data.Morphosyntax.Tagset (Tagset)
 import qualified Data.Morphosyntax.Raw as Raw
 import qualified Data.Morphosyntax.Canonical as Cano
+import Text.Morphosyntax.Tag (showTag)
 
 parsePlain :: Tagset -> L.Text -> [Cano.SentMlt]
 parsePlain tagset = map (Raw.toCanoSentMlt tagset) . parsePlainR
@@ -69,3 +77,53 @@ parseSpace "none"    = NoSpace
 parseSpace "space"   = Space
 parseSpace "newline" = NewLine
 parseSpace xs        = error ("parseSpace: " ++ L.unpack xs)
+
+-- | Printing.
+
+-- | An infix synonym for 'mappend'.
+(<>) :: Monoid m => m -> m -> m
+(<>) = mappend
+{-# INLINE (<>) #-}
+
+showPlain :: Tagset -> [Cano.SentMlt] -> L.Text
+showPlain tagset =
+    L.toLazyText . mconcat  . map (\xs -> buildSent tagset xs <> "\n")
+
+showSent :: Tagset -> Cano.SentMlt -> L.Text
+showSent tagset = L.toLazyText . buildSent tagset
+
+showWord :: Tagset -> (Cano.Word, Cano.Multi) -> L.Text
+showWord tagset = L.toLazyText . buildWord tagset
+
+buildSent :: Tagset -> Cano.SentMlt -> L.Builder
+buildSent tagset = mconcat . map (buildWord tagset)
+
+buildWord :: Tagset -> (Cano.Word, Cano.Multi) -> L.Builder
+buildWord tagset (word, multi)
+    =  L.fromLazyText (Cano.orth word) <> "\t"
+    <> buildSpace (Cano.space word) <> "\n"
+    <> buildInterps tagset multi interps
+  where
+    -- | We hadle the special case here, when the set of choices
+    -- is not a subset of the set of interpretations. It is
+    -- convenient, because after guessing we will have forms with
+    -- "None" base form, which will cause the situation described.
+    interps = Cano.interps word ++
+        [x | (x, _) <- multi, not (x `S.member` interpSet)]
+    interpSet = S.fromList (Cano.interps word)
+
+buildInterps :: Tagset -> Cano.Multi -> [Cano.Interp] -> L.Builder
+buildInterps tagset multi interps = mconcat
+    [ "\t" <> L.fromLazyText (Cano.base x) <>
+      "\t" <> L.fromLazyText (showTag tagset $ Cano.tag x) <>
+      if x `S.member` choice
+        then "\tdisamb\n"
+        else "\n"
+    | x <- interps ]
+  where
+    choice = S.fromList $ map fst multi
+
+buildSpace :: Cano.Space -> L.Builder
+buildSpace NoSpace  = "none"
+buildSpace Space    = "space"
+buildSpace NewLine  = "newline"
--- Text/Morphosyntax/Tag.hs
+++ Text/Morphosyntax/Tag.hs
@@ -1,14 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternGuards #-}
 
 module Text.Morphosyntax.Tag
 ( parseTag
+, showTag
 ) where
 
 import qualified Data.Set as S
 import qualified Data.Map as M
 import qualified Data.Text.Lazy as L
+import Data.Maybe (catMaybes)
 
 import Data.Morphosyntax.Tagset
+
+import Debug.Trace (trace)
 
 parseTag :: Tagset -> L.Text -> Tag
 parseTag tagset inp =
@@ -28,3 +33,16 @@ parseTag tagset inp =
         ++ L.unpack inp
     parseRule [] xs = error $ "parseRule: input too long in tag "
         ++ L.unpack inp
+
+showTag :: Tagset -> Tag -> L.Text
+showTag tagset tag =
+    -- trace (L.unpack result) result
+    result
+  where
+    result =  L.intercalate ":" (pos tag : catMaybes attrVals)
+    attrVals = map showAttr $ rule tagset (pos tag)
+    showAttr (attr, opt)
+        | Just x <- M.lookup attr (atts tag) = Just x
+        | opt == True = Nothing
+        | otherwise = error $
+            "showTag: no value for mandatory attribute " ++ L.unpack attr
